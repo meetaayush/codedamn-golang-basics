08abadceebb80a78c92d2b3b581af7614ea6c754
Ayush <ayush@autonom8.com>
2023-03-08 18:20:23 +0530

    Starter code for section4_lab7

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	arr := [10]int{10, 20, 30, 50}
-	fmt.Println(arr[0])
-	fmt.Println(arr[2])
-	fmt.Println(arr[4])
-	fmt.Println(arr[8])
-	fmt.Println(arr[10])
+	arr := [5]string{"a", "b", "c"}
+	for index, element := range arr {
+		fmt.Println(index, "->", element)
+	}
 }
