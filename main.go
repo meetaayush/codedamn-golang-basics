d1430626028754f6f835fd9febe53f6de255652a
Ayush <ayush@autonom8.com>
2023-03-08 18:12:24 +0530

    Starter code for section4 lab6

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	arr := [10]int{10, 20, 30, 50}
-	fmt.Println(arr)
-	fmt.Println(len(arr))
+	fmt.Println(arr[0])
+	fmt.Println(arr[2])
+	fmt.Println(arr[4])
+	fmt.Println(arr[8])
+	fmt.Println(arr[10])
 }
