da580ed460bc5cecd73bcc70b23ae2cbd62cd412
Ayush <ayush@autonom8.com>
2023-03-14 18:13:25 +0530

    Starter code for section4_lab16

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	ascii_codes := map[string]int{}
-	ascii_codes["A"] = 65
-	_, found := ascii_codes["B"]
-	if found {
-		fmt.Println("key B was not found")
-	}
+	// Write your code here
+	// Follow the steps below and choose the correct option
+
+	// step 1: create a map using make() function with key data type as string, and value data type as int.
+	// step 2: add the following key_value pairs to it
+	// step 3: ("A", 65)
+	// step 4: ("F", 70)
+	// step 5: ("K", 75)
+	// step 6: delete the key "F"
+	// step 7: print the map
 }
