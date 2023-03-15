a32c7b727dc598a354ddfa722e216e1cba87328e
Ayush <ayush@autonom8.com>
2023-03-15 12:05:28 +0530

    Starter code for section4 lab17

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-func main() {
-	// Write your code here
-	// Follow the steps below and choose the correct option
+import "fmt"
 
-	// step 1: create a map using make() function with key data type as string, and value data type as int.
-	// step 2: add the following key_value pairs to it
-	// step 3: ("A", 65)
-	// step 4: ("F", 70)
-	// step 5: ("K", 75)
-	// step 6: delete the key "F"
-	// step 7: print the map
+func main() {
+	my_arr := [5]string{"one", "two", "three"}
+	slice := my_arr[:3]
+	my_map := make(map[int]string)
+	for i, el := range slice {
+		my_map[i+1] = el
+	}
+	fmt.Println(my_map)
 }
