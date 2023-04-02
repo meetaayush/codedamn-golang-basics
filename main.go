5fd26215bff2d789f816e3fc3499f3d41bd8eae5
Ayush <ayush@autonom8.com>
2023-04-02 15:49:05 +0530

    Starter code for section8 lab2

--- main.go
+++ main.go
@@ -2,24 +2,11 @@ package main
 
 import "fmt"
 
-type Movie struct {
-	name    string
-	summary string
-	rating  float32
-}
-
-func (m Movie) getSummary() {
-	m.summary = "summary"
-}
-
-func (m *Movie) increaseRating() {
-	m.rating += 1
+type Circle struct {
+	radius float64
 }
 
 func main() {
-	mov := Movie{"xyz", "", 2.1}
-	fmt.Printf("%+v", mov)
-	mov.increaseRating()
-	mov.getSummary()
-	fmt.Printf("%+v", mov)
+	c := Circle{radius: 5.365}
+	fmt.Println(c.area())
 }
